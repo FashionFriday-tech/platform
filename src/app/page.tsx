7719a1a81bb1dd985a6ae7c33207b888f359c60a
ajmal faris <ajmalfaris.me@gmail.com>
2025-12-21 14:50:35 +0530

    feat(home-page): add brand grid

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import BrandScroll from "@/components/sections/home/BrandScroll";
 import FeaturedCategories from "@/components/sections/home/FeaturedCatagories";
 import Hero from "@/components/sections/home/Hero";
 import NewArrivals from "@/components/sections/home/NewArrivals";
+import ShopByBrand from "@/components/sections/home/ShopByBrands";
 import TrendingSection from "@/components/sections/home/TrendingSection";
 
 
@@ -14,6 +15,7 @@ export default function Home() {
       <FeaturedCategories />
       <TrendingSection />
       <NewArrivals />
+      <ShopByBrand />
     </div>
   );
 }
