0a22260cd2907ad1574dc4cbdc56b1f90471432d
ajmal faris <ajmalfaris.me@gmail.com>
2025-12-21 22:48:47 +0530

    feat (home) : add influecer section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import BrandScroll from "@/components/sections/home/BrandScroll";
 import FeaturedCategories from "@/components/sections/home/FeaturedCatagories";
 import FreeShipping from "@/components/sections/home/FreeShipping";
 import Hero from "@/components/sections/home/Hero";
+import InfluencerCarousel from "@/components/sections/home/InfluencerCarousel";
 import NewArrivals from "@/components/sections/home/NewArrivals";
 import ShopByBrand from "@/components/sections/home/ShopByBrands";
 import TrendingSection from "@/components/sections/home/TrendingSection";
@@ -16,6 +17,7 @@ export default function Home() {
       <NewArrivals />
       <FreeShipping />
       <ShopByBrand />
+      <InfluencerCarousel />
     </div>
   );
 }
