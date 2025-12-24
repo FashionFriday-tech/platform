6cfdb45e43f53129e43d6079bac1629d0b40aaed
ajmal faris <ajmalfaris.me@gmail.com>
2025-12-24 14:52:10 +0530

    feat (home hero) : update the hero section by changing grid formate and grid sizes

--- src/components/sections/home/Hero.tsx
+++ src/components/sections/home/Hero.tsx
@@ -6,10 +6,47 @@ export default function Hero() {
   return (
     // OUTER SECTION: Padding around the grid
     <section className="w-full p-4 lg:p-6 bg-white">
-      <div className="grid grid-cols-1 lg:grid-cols-12 gap-4 lg:gap-6 min-h-[80vh] lg:h-[80vh]">
-        <div className="lg:col-span-7 flex flex-col gap-4 lg:gap-6 h-full">
+      <div className="grid grid-cols-1 lg:grid-cols-12 gap-4 min-h-[80vh] mt-18 lg:h-[85vh]">
+        
+
+        <div className="lg:col-span-6 relative h-125 lg:h-full rounded-3xl overflow-hidden group">
+          <Image
+            src="/hero/main-hero.png"
+            alt="New Season Collection"
+            fill
+            priority
+            className="object-cover object-bottom"
+          />
+
+          <div className="absolute inset-0 p-6 lg:p-8 flex flex-col justify-between">
+            <div className="self-end"></div>
+
+            <div className="flex items-end justify-between w-full">
+              <div className="flex gap-2 lg:gap-3">
+                <button className="bg-white pl-4 pr-2 py-2 rounded-full flex items-center gap-3 hover:bg-gray-100 transition-colors shadow-sm">
+                  <span className="text-xs font-bold uppercase tracking-wider">
+                    Learn More
+                  </span>
+                  <div className="w-8 h-8 bg-black rounded-full flex items-center justify-center">
+                    <ArrowDown className="w-4 h-4 text-white" />
+                  </div>
+                </button>
+
+                <button className="bg-white pl-4 pr-2 py-2 rounded-full flex items-center gap-3 hover:bg-gray-100 transition-colors shadow-sm">
+                  <span className="text-xs font-bold uppercase tracking-wider">
+                    Contact
+                  </span>
+                  <div className="w-8 h-8 bg-black rounded-full flex items-center justify-center">
+                    <Mail className="w-4 h-4 text-white" />
+                  </div>
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="lg:col-span-6 flex flex-col gap-4 h-full">
           <div className="grow p-8 lg:p-12 bg-black rounded-3xl flex flex-col justify-center relative overflow-hidden">
-            <div className="z-10 relative flex justify-between items-center">
+            <div className="z-10 relative flex justify-between gap-10 items-center">
               <h1 className="text-4xl lg:text-5xl font-black uppercase leading-[0.9] tracking-tight text-gray-100">
                 <div className="flex gap-4">
                   For <ArrowRight className="w-10 h-10 stroke-[1.5px] text-gray-100" />
@@ -25,7 +62,7 @@ export default function Hero() {
               </p>
             </div>
           </div>
-          <div className="grid grid-cols-2 gap-4 lg:gap-6 h-50 lg:h-[35%] shrink-0">
+          <div className="grid grid-cols-2 gap-4 lg:gap-6 h-50 lg:h-[50%] shrink-0">
             <div className="relative rounded-2xl overflow-hidden group w-full h-full">
               <Image
                 src="/hero/hero-grid1.png"
@@ -51,52 +88,6 @@ export default function Hero() {
                 <span className="text-xs font-bold uppercase tracking-wider">
                   #Insulated
                 </span>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div className="lg:col-span-5 relative h-125 lg:h-full rounded-3xl overflow-hidden group">
-          <Image
-            src="/hero/main-hero.png"
-            alt="New Season Collection"
-            fill
-            priority
-            className="object-cover object-bottom"
-          />
-
-          <div className="absolute inset-0 p-6 lg:p-8 flex flex-col justify-between">
-            <div className="self-end"></div>
-
-            <div className="flex items-end justify-between w-full">
-              <Link href="/shop/new" className="group/btn relative z-10">
-                <div className="w-20 h-20 lg:w-24 lg:h-24 bg-white/30 backdrop-blur-md flex items-center justify-center rounded-full transition-transform duration-300 group-hover/btn:scale-110 group-hover/btn:bg-white">
-                  <span className="text-xs font-bold text-white uppercase group-hover/btn:text-black text-center leading-tight">
-                    Shop
-                    <br />
-                    Now
-                  </span>
-                </div>
-              </Link>
-
-              <div className="flex gap-2 lg:gap-3">
-                <button className="bg-white pl-4 pr-2 py-2 rounded-full flex items-center gap-3 hover:bg-gray-100 transition-colors shadow-sm">
-                  <span className="text-xs font-bold uppercase tracking-wider">
-                    Learn More
-                  </span>
-                  <div className="w-8 h-8 bg-black rounded-full flex items-center justify-center">
-                    <ArrowDown className="w-4 h-4 text-white" />
-                  </div>
-                </button>
-
-                <button className="bg-white pl-4 pr-2 py-2 rounded-full flex items-center gap-3 hover:bg-gray-100 transition-colors shadow-sm">
-                  <span className="text-xs font-bold uppercase tracking-wider">
-                    Contact
-                  </span>
-                  <div className="w-8 h-8 bg-black rounded-full flex items-center justify-center">
-                    <Mail className="w-4 h-4 text-white" />
-                  </div>
-                </button>
               </div>
             </div>
           </div>
