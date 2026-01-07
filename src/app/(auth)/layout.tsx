29ff6029d192dbf645246c213b13bad0af6414dc
ajmal faris <ajmalfaris.me@gmail.com>
2026-01-07 21:39:36 +0530

    feat(auth): add global "Exit to Store" navigation to AuthLayout
    - Implemented absolute-positioned home link with brand logo.
    - Added hover effects for better UX/UI consistency.
    - Ensured the button persists across all auth sub-routes.

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import "@/app/globals.css";
 import ImageCarousel from "@/components/ui/sections/ImageCarousel";
+import { ArrowLeft } from "lucide-react";
+import Link from "next/link";
+import Image from "next/image";
 
 export const metadata: Metadata = {
   title: "Sign in | Fashion Friday",
@@ -23,6 +26,25 @@ export default function AuthLayout({
 }) {
   return (
     <div className="h-screen flex w-full text-white selection:bg-black selection:text-white items-center justify-center bg-black p-4">
+      <div className="absolute top-6 right-6 z-50">
+        <Link
+          href="/"
+          className="group flex  items-center gap-2 text-zinc-500 hover:text-white transition-all duration-300"
+        >
+          <span className="text-[10px] font-bold tracking-[0.2em] uppercase">
+            Exit to Store
+          </span>
+          <div className="flex items-center justify-center w-10 h-10 rounded-full border border-zinc-800 group-hover:border-white group-hover:bg-white group-hover:text-black transition-all">
+            <Image
+              src="/images/logos/ff-logo.png"
+              alt="Fashion Friday Logo"
+              width={25}
+              height={25}
+              className="invert mt-0.5 group-hover:invert-0"
+            />
+          </div>
+        </Link>
+      </div>
       <div className="flex w-full h-full overflow-hidden">
         {/* SHARED VISUAL SIDE */}
         <div className="hidden lg:flex w-1/2 p-4">
