65dff5e6a0d210d1a98719f4712f76f40233ae95
ajmal faris <ajmalfaris.me@gmail.com>
2026-01-09 09:45:59 +0530

    feat(theme): add page component with AnimatedThemeToggler and remove ThemeToggle

---
+++ src/app/(account)/account/settings/page.tsx
@@ -0,0 +1,13 @@
+import { AnimatedThemeToggler } from '@/components/ui/magicUi/animated-theme-toggler'
+import React from 'react'
+
+
+function page() {
+  return (
+    <div className='min-h-screen flex items-center justify-center'>
+      <AnimatedThemeToggler />
+    </div>
+  )
+}
+
+export default page
