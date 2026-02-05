dfee9393602d3498be9762897a2e023d1041402a
ajmal faris <ajmalfaris.me@gmail.com>
2026-02-05 17:03:49 +0530

    feat: add shared icons module

---
+++ libs/ui/src/icons/index.ts
@@ -0,0 +1,15 @@
+import { 
+  HiOutlineAdjustmentsHorizontal, 
+  HiOutlineArrowsUpDown 
+} from "react-icons/hi2";
+import { 
+  IoPlaySharp, 
+  IoSquareSharp, 
+  IoChevronDownOutline 
+} from "react-icons/io5";
+
+export const FilterIcon = HiOutlineAdjustmentsHorizontal;
+export const SortIcon = HiOutlineArrowsUpDown;
+export const PlayIcon = IoPlaySharp;
+export const StopIcon = IoSquareSharp;
+export const ChevronIcon = IoChevronDownOutline;
