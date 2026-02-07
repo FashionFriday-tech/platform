26dd200696febc35895fb4c0b5253db1da53cc60
ajmal faris <ajmalfaris.me@gmail.com>
2026-02-07 16:15:14 +0530

    feat(review-schemas): add Review schema with validation rules for reviews

---
+++ packages/types/src/review.ts
@@ -0,0 +1,15 @@
+import { z } from 'zod';
+
+export const ReviewSchema = z.object({
+  id: z.string().uuid(),
+  productId: z.string().uuid(), 
+  userId: z.string().uuid(),
+  userName: z.string().min(4),
+  userAvatar: z.string().optional(), 
+  rating: z.number().min(1).max(5),
+  comment: z.string(),
+  productImage: z.string().url().optional(), 
+  createdAt: z.date(),
+});
+
+export type Review = z.infer<typeof ReviewSchema>;
