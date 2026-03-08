813b1e5606cec8610fe68c318af7cb905edad7bc
ajmal faris <ajmalfaris.me@gmail.com>
2026-03-09 03:18:14 +0530

    feat(prisma): add PrismaService and its unit tests

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
 import { PrismaModule } from './database/prisma.module';
+import { PrismaService } from './prisma/prisma.service';
 
 @Module({
   imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule],
+  providers: [PrismaService],
 })
 export class AppModule {}
