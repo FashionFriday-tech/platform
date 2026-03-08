8ca5706846d95e5d6e79b2a147b10e5139918ebc
ajmal faris <ajmalfaris.me@gmail.com>
2026-03-09 03:04:49 +0530

    feat(api): add environment configuration and validation, update main entry point

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,9 +1,14 @@
 import { NestFactory } from '@nestjs/core';
+import { ZodValidationPipe } from 'nestjs-zod';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3001);
+
+  app.useGlobalPipes(new ZodValidationPipe());
+
+  await app.listen(4000);
 }
+
 void bootstrap();
