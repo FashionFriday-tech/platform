fd1a65584764edb96bce6d25fa104f4fa31ecda1
ajmal faris <ajmalfaris.me@gmail.com>
2026-03-12 12:18:21 +0530

    feat(prisma): implement PrismaService with onModuleInit for database connection

--- apps/api/src/prisma/prisma.service.ts
+++ apps/api/src/prisma/prisma.service.ts
@@ -1,4 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { prisma } from '@ff/database';
+import { Injectable, OnModuleInit } from '@nestjs/common';
 
 @Injectable()
-export class PrismaService {}
+export class PrismaService implements OnModuleInit {
+  async onModuleInit() {
+    await prisma.$connect();
+  }
+}
