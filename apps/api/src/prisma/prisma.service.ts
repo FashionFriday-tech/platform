4235f325e8fb28d46b32ff447f6f93789f77b4b4
ajmal faris <ajmalfaris.me@gmail.com>
2026-04-08 23:57:24 +0530

    refactor(api): remove deprecated prisma service

--- apps/api/src/prisma/prisma.service.ts
+++
@@ -1,9 +0,0 @@
-import { prisma } from '@ff/database';
-import { Injectable, OnModuleInit } from '@nestjs/common';
-
-@Injectable()
-export class PrismaService implements OnModuleInit {
-  async onModuleInit() {
-    await prisma.$connect();
-  }
-}
