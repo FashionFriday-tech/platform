e5381d71243f1921cf626ef42c10804b15b68f76
ajmal faris <ajmalfaris.me@gmail.com>
2026-04-08 23:57:11 +0530

    refactor(api): configure PrismaModule in AppModule

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
+import { AppController } from './app.controller';
 import { PrismaModule } from './database/prisma.module';
-import { PrismaService } from './prisma/prisma.service';
+import { PrismaService } from './database/prisma.service';
 
 @Module({
   imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule],
+  controllers: [AppController],
   providers: [PrismaService],
 })
 export class AppModule {}
