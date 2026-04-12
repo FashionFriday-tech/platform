894099bd3633361af30b28bae6ba77d257c319e5
ajmal faris <ajmalfaris.me@gmail.com>
2026-04-12 15:30:21 +0530

    feat(api): register AuthModule and ThrottlerModule in App

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,12 +1,24 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { ThrottlerModule } from '@nestjs/throttler';
 
 import { AppController } from './app.controller';
 import { PrismaModule } from './database/prisma.module';
 import { PrismaService } from './database/prisma.service';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    PrismaModule,
+    AuthModule,
+    ThrottlerModule.forRoot([
+      {
+        ttl: 60000,
+        limit: 10,
+      },
+    ]),
+  ],
   controllers: [AppController],
   providers: [PrismaService],
 })
