cb934404f68b25ad2c78df2a6a69301c4f1cfc91
ajmal faris <ajmalfaris.me@gmail.com>
2026-04-12 15:31:59 +0530

    feat(api): add AuthModule definition

---
+++ apps/api/src/modules/auth/auth.module.ts
@@ -0,0 +1,31 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule, JwtModuleOptions } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+
+import { PrismaModule } from '../../database/prisma.module';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { JwtStrategy } from './strategies/jwt.strategy';
+import { RefreshTokenStrategy } from './strategies/refresh.strategy';
+
+@Module({
+  imports: [
+    PrismaModule,
+    PassportModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService): JwtModuleOptions => ({
+        secret: configService.get<string>('JWT_SECRET', 'super-secret-default-key'),
+        signOptions: {
+          expiresIn: configService.get<string>('JWT_EXPIRES_IN', '30d'),
+        },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+  controllers: [AuthController],
+  providers: [AuthService, JwtStrategy, RefreshTokenStrategy],
+  exports: [AuthService],
+})
+export class AuthModule {}
