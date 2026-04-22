07386e5f3856f29874f6809cf041fdf55474a7e5
ajmal faris <ajmalfaris.me@gmail.com>
2026-04-22 10:07:00 +0530

    feat(categories): export gender/category components in index barrel

--- apps/web/src/features/categories/index.ts
+++ apps/web/src/features/categories/index.ts
@@ -1,2 +1,4 @@
 export { CategoriesLanding } from './components/categories-landing';
 export { CategoryCatalog } from './components/category-catalog';
+export { GenderLanding } from './components/gender-landing';
+export { CategoryPage } from './components/category-page';
