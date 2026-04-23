826d81da967ad67235aa3e609996cf5fd8a7f0c4
ajmal faris <ajmalfaris.me@gmail.com>
2026-04-23 10:02:00 +0530

    feat(brand): add public exports index barrel

---
+++ apps/web/src/features/brand/index.ts
@@ -0,0 +1,4 @@
+export { BrandCard } from './components/BrandCard';
+export { BrandsPage } from './components/brands-page';
+export { BrandCatalog } from './components/brand-catalog';
+export type { Brand, BrandCategory } from './types/brand';
