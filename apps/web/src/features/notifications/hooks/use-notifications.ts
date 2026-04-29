b8fc0fa937a524d713068727ac5341a0faeb2e7c
ajmal faris <ajmalfaris.me@gmail.com>
2026-04-29 12:35:00 +0530

    refactor(hooks): prepend 'use client' boundary in use-notifications.ts

--- apps/web/src/features/notifications/hooks/use-notifications.ts
+++ apps/web/src/features/notifications/hooks/use-notifications.ts
@@ -1,3 +1,4 @@
+'use client';
 import { useMemo } from 'react';
 
 import { MOCK_NOTIFICATIONS } from '../data';
@@ -15,3 +16,4 @@ export function useNotifications(type: TabType) {
 
   return { notifications };
 }
+
