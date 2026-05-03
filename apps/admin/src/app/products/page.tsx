d8cf1b2f55b7861ae7290e30cfd370b781679719
ajmal faris <ajmalfaris.me@gmail.com>
2026-05-03 12:00:00 +0530

    Update page.tsx

---
+++ apps/admin/src/app/products/page.tsx
@@ -0,0 +1,9 @@
+import { ProductListView } from "@/features/products";
+
+export default function ProductsPage() {
+  return (
+    <div className="w-full h-full flex flex-col min-h-0">
+      <ProductListView />
+    </div>
+  );
+}
