8622a1f633b980d0398f8d409ae121409b6d3db3
ajmal faris <ajmalfaris.me@gmail.com>
2026-05-08 12:00:00 +0530

    chore: add react-simple-maps dependency

---
+++ apps/admin/src/app/analytics/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { AnalyticsView } from '../../features/analytics';
+
+export const metadata = {
+  title: 'Advanced Analytics | Fashion Friday',
+};
+
+export default function AnalyticsPage() {
+  return <AnalyticsView />;
+}
