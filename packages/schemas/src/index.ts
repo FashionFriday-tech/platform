c36ec1d62b3857d3b12e9cfc8483ca6bc366e2af
ajmal faris <ajmalfaris.me@gmail.com>
2026-05-21 13:18:00 +0530

    refactor(schemas): configure index.ts for feature requirements

--- packages/schemas/src/index.ts
+++ packages/schemas/src/index.ts
@@ -3,5 +3,6 @@ export * from './auth.schema';
 export * from './user';
 export * from './address';
 export * from './review.schema';
+export * from './category.schema';
 
 export * from './constants';
