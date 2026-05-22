42fadc846f56524ffc65d88034abb6072d0b5ace
ajmal faris <ajmalfaris.me@gmail.com>
2026-05-22 11:45:00 +0530

    feat(api): register brands module in app module

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -6,10 +6,11 @@ import { AppController } from './app.controller';
 import { PrismaModule } from './database/prisma.module';
 import { PrismaService } from './database/prisma.service';
 import { AuthModule } from './modules/auth/auth.module';
-
+import { BrandsModule } from './modules/brands/brands.module';
+import { CategoriesModule } from './modules/categories/categories.module';
 import { ProductsModule } from './modules/products/products.module';
 import { UploadModule } from './modules/upload/upload.module';
-import { CategoriesModule } from './modules/categories/categories.module';
+
 
 @Module({
   imports: [
