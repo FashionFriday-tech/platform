a3bdd9c047b1cc919c3dbd4bfaa70023016145ae
ajmal faris <ajmalfaris.me@gmail.com>
2026-05-22 17:46:00 +0530

    chore: update collections.module.ts

---
+++ apps/api/src/modules/collections/collections.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { PrismaModule } from '../../database/prisma.module';
+import { AdminCollectionsController, PublicCollectionsController } from './collections.controller';
+import { CollectionsRepository } from './collections.repository';
+import { CollectionsService } from './collections.service';
+
+@Module({
+  imports: [PrismaModule],
+  controllers: [AdminCollectionsController, PublicCollectionsController],
+  providers: [CollectionsService, CollectionsRepository],
+  exports: [CollectionsService],
+})
+export class CollectionsModule {}
