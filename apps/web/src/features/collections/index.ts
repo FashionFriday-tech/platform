81097601666e6d88c87df8874c0a12aa99a3cf66
ajmal faris <ajmalfaris.me@gmail.com>
2026-06-24 11:42:00 +0530

    style(web): format collections exports

--- apps/web/src/features/collections/index.ts
+++ apps/web/src/features/collections/index.ts
@@ -1,4 +1,3 @@
-export * from './types';
-export { CollectionsPage } from './components/collections-page';
 export { CollectionDetails } from './components/collection-details';
-
+export { CollectionsPage } from './components/collections-page';
+export type * from './types';
