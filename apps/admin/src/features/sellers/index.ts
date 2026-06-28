a0d8fa680c4a1e25c149605707be9e3c0113b826
ajmal faris <ajmalfaris.me@gmail.com>
2026-06-28 15:10:00 +0530

    refactor(admin): remove unused modal exports in sellers barrel

--- apps/admin/src/features/sellers/index.ts
+++ apps/admin/src/features/sellers/index.ts
@@ -1,6 +1,5 @@
 export * from './components/AddSellerModal';
 export * from './components/SellerDetailsFeature';
-export * from './components/SellerDetailsModal';
 export * from './components/SellerOrdersList';
 export * from './components/SellersFeature';
 export * from './components/SellersFilterBar';
