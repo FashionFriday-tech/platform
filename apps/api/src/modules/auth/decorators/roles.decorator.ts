6899bfa1bf20256cc48150ad9a2c809fc99a4138
ajmal faris <ajmalfaris.me@gmail.com>
2026-06-28 09:35:00 +0530

    refactor(admin): remove unused ThemeToggle component

--- apps/api/src/modules/auth/decorators/roles.decorator.ts
+++
@@ -1,5 +0,0 @@
-import { type UserRole } from '@ff/database';
-import { SetMetadata } from '@nestjs/common';
-
-export const ROLES_KEY = 'roles';
-export const Roles = (...roles: UserRole[]) => SetMetadata(ROLES_KEY, roles);
